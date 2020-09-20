b20eb1a1ffb6c50835cc46663160724e5fd19e9e
Margo <margokomilfo@mail.ru>
2020-09-20 20:21:01 +0200

    1st lesson

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,48 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
+import { Todolist } from './components/Todolist';
+
+
+export type TasksType = {
+    id: number
+    title: string
+    isDone: boolean
+}
+
 
 function App() {
+    const task1: Array<TasksType> = [
+        {id: 1, isDone: true, title: 'Html'},
+        {id: 2, isDone: false, title: 'CSS'},
+        {id: 3, isDone: true, title: 'JS'},
+        {id: 4, isDone: true, title: 'tasks from Ignat'},
+        {id: 5, isDone: true, title: 'Social Network'},
+        {id: 6, isDone: false, title: 'CodeWars'}
+    ]
+    const task2: Array<TasksType> = [
+        {id: 1, isDone: true, title: 'Путь самурая'},
+        {id: 2, isDone: false, title: 'Реакт- кабзда как просто'},
+        {id: 3, isDone: false, title: 'SocialNetwork'},
+        {id: 4, isDone: true, title: 'Tasks from Ignat'},
+        {id: 5, isDone: false, title: 'documentation'},
+        {id: 6, isDone: false, title: 'x3'}
+    ]
+    const task3: Array<TasksType> = [
+        {id: 1, isDone: true, title: 'Codewars.com'},
+        {id: 2, isDone: false, title: 'native JS'},
+        {id: 3, isDone: false, title: 'code.mu'},
+        {id: 4, isDone: true, title: 'lessons on Sundays'},
+        {id: 5, isDone: false, title: 'documentation'},
+        {id: 6, isDone: false, title: 'x3'}
+    ]
     return (
         <div className="App">
-            <div>
-                <h3>What to learn</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
-            </div>
+            <Todolist title='What to learn' tasks = {task1}/>
+            <Todolist title='React' tasks = {task2}/>
+            <Todolist title='JS' tasks = {task3}/>
         </div>
     );
 }
 
 export default App;
+
---
+++ src/components/Todolist.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import {TasksType} from "../App";
+
+
+type TodolistPropsType = {
+    title: string
+    tasks: Array<TasksType>
+}
+
+
+export function Todolist (props: TodolistPropsType) {
+    debugger
+    return (
+        <div>
+            <h3>{props.title}</h3>
+            <div>
+                <input/>
+                <button>+</button>
+            </div>
+            <ul>
+                <li>
+                    <input type="checkbox" checked={props.tasks[0].isDone}/>
+                    <span>{props.tasks[0].title}</span>
+                </li>
+                <li>
+                    <input type="checkbox" checked={props.tasks[1].isDone}/>
+                    <span>{props.tasks[1].title}</span>
+                </li>
+                <li>
+                    <input type="checkbox" checked={props.tasks[2].isDone}/>
+                    <span>{props.tasks[2].title}</span>
+                </li>
+                <li>
+                    <input type="checkbox" checked={props.tasks[3].isDone}/>
+                    <span>{props.tasks[3].title}</span>
+                </li>
+                <li>
+                    <input type="checkbox" checked={props.tasks[4].isDone}/>
+                    <span>{props.tasks[4].title}</span>
+                </li>
+                <li>
+                    <input type="checkbox" checked={props.tasks[5].isDone}/>
+                    <span>{props.tasks[5].title}</span>
+                </li>
+            </ul>
+            <div>
+                <button>All</button>
+                <button>Active</button>
+                <button>Completed</button>
+            </div>
+        </div>
+    )
+}
+
+export default Todolist
