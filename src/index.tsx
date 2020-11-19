b8d0b14df70142c01215732d668dd90d2ab8503c
Margokomilfo-dev <margokomilfo@mail.ru>
2020-11-19 22:34:03 +0100

    10. add AppWithUseReducer

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import AppWithUseReducer from "./AppWithUseReducer";
 
 
-ReactDOM.render(<App />,  document.getElementById('root'));
+ReactDOM.render(<AppWithUseReducer />,  document.getElementById('root'));
