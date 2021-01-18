7b2e5fab20cd2bd45d4c85d40b92172afa770052
Margokomilfo-dev <margokomilfo@mail.ru>
2021-01-18 22:47:45 +0100

    16 - form - styles

--- src/features/Login/Login.tsx
+++ src/features/Login/Login.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Checkbox, FormControl, FormControlLabel, FormGroup, FormLabel, TextField, Button, Grid} from '@material-ui/core'
-import { useFormik } from 'formik'
+import {useFormik} from 'formik'
+import s from './Login.module.css'
 
 type FormikErrorType = {
     email?: string
@@ -9,21 +10,21 @@ type FormikErrorType = {
 }
 
 export const Login = () => {
-    const validate = (values: {email: string,password: string,rememberMe: boolean}) => {
-        const errors:FormikErrorType= {};
+    const validate = (values: { email: string, password: string, rememberMe: boolean }) => {
+        const errors: FormikErrorType = {}
         if (!values.password) {
-            errors.password = 'Required';
+            errors.password = 'Required'
         } else if (values.password.length < 5) {
-            errors.password = 'Must be 5 characters or more';
+            errors.password = 'Must be 5 characters or more'
         }
         if (!values.email) {
-            errors.email = 'Required';
+            errors.email = 'Required'
         } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-            errors.email = 'Invalid email address';
+            errors.email = 'Invalid email address'
         }
 
-        return errors;
-    };
+        return errors
+    }
 
     const formik = useFormik({
         initialValues: {
@@ -33,7 +34,7 @@ export const Login = () => {
         },
         validate,
         onSubmit: values => {
-            alert(JSON.stringify(values));
+            alert(JSON.stringify(values))
         },
     })
 
@@ -41,15 +42,15 @@ export const Login = () => {
         <Grid item xs={4}>
             <form onSubmit={formik.handleSubmit}>
                 <FormControl>
-                    <FormLabel>
-                        <p>To log in get registered
+                    <FormLabel className={s.info}>
+                        <p>To log in get registered&nbsp;
                             <a href={'https://social-network.samuraijs.com/'}
                                target={'_blank'}>here
                             </a>
                         </p>
                         <p>or use common test account credentials:</p>
-                        <p>Email: free@samuraijs.com</p>
-                        <p>Password: free</p>
+                        <p><span>Email: </span>Email: free@samuraijs.com</p>
+                        <p><span>Password: </span> free</p>
                     </FormLabel>
                     <FormGroup>
                         <TextField
@@ -60,7 +61,8 @@ export const Login = () => {
                             value={formik.values.email}
                             onBlur={formik.handleBlur}
                         />
-                        {formik.touched.email && formik.errors.email ? <div>{formik.errors.email}</div> : null}
+                        {formik.touched.email && formik.errors.email ?
+                            <div className={s.error}>{formik.errors.email}</div> : null}
                         <TextField
                             type="password"
                             label="Password"
@@ -70,10 +72,11 @@ export const Login = () => {
                             value={formik.values.password}
                             onBlur={formik.handleBlur}
                         />
-                        {formik.touched.password && formik.errors.password ? <div>{formik.errors.password}</div> : null}
+                        {formik.touched.password && formik.errors.password ?
+                            <div className={s.error}>{formik.errors.password}</div> : null}
                         <FormControlLabel
                             label={'Remember me'}
-                            control={<Checkbox />}
+                            control={<Checkbox/>}
                             name="rememberMe"
                             onChange={formik.handleChange}
                             value={formik.values.rememberMe}
